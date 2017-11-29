5f190e2a0a75e7cbabbd8de17ef8313e953273d4
Pavel Samarkin <samarkinpa@gmail.com>
2017-11-28 21:38:07 -0800

    Add night mode

--- ClickWheel/ViewController.swift
+++ ClickWheel/ViewController.swift
@@ -1,4 +1,19 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var clickWheel: ClickWheelView!
+    private var nightMode: Bool = false
+    @IBAction func nightModeTap(_ sender: UIButton) {
+        nightMode = !nightMode
+        if nightMode {
+            view.backgroundColor = .black
+            clickWheel.backgroundColor = .black
+            clickWheel.wheelColor = .white
+        }
+        else {
+            view.backgroundColor = .white
+            clickWheel.backgroundColor = .white
+            clickWheel.wheelColor = .black
+        }
+    }
 }
